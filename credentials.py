747086e83f191e28a7f4ab79e7f9798630fb4b41
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2019-11-11 20:34:28 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-client_id = 'Jg6YWjh5K8Xm' 
-client_secret = '5RMZIA5KX2DRFMRZTIZ45C47ENZFV7M6'
+client_id = 'Enter_Client_ID_Here' 
+client_secret = 'Enter_Client_Secret_Here'
 client_url = 'wss://testapp.deribit.com/ws/api/v2'
